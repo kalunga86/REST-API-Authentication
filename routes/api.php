8f301c746fd38f56afd8352858d154b53ca593f3
Keith Alunga <kalunga86@gmail.com>
2024-05-20 20:00:12 +0300

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
+
+// Register
+Route::post('/register', [ApiController::class, 'register']);
+
+// Login
+Route::post('/login', [ApiController::class, 'login']);
+
+Route::group([
+    'middleware' => ['auth:sanctum']
+], function(){
+    // Profile
+    Route::get('/profile', [ApiController::class, 'profile']);
+
+    // Profile
+    Route::get('/logout', [ApiController::class, 'logout']);
+});
+
+/* Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum'); */
